c2bb9df5947afa1909ec13ffd7434ea1aebdd92b
Your Name <you@example.com>
2024-06-21 04:08:05 +0100

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,1 @@
-#include <stdio.h>
-
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
-gcc main.c -o
+#include <stdio.h>\n\nint main(void) {\n    puts("Programming is like building a multilingual puzzle");\n    return (0);\n}
