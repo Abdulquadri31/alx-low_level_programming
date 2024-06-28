a1601f0e2904bc2c4a14d932c4a35584112b470f
Abdulquadri31 <abdulquadriakinde@gmai.com>
2024-06-28 21:29:02 +0100

    print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,29 +1,23 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * more_numbers - Prints the numbers from 0 to 14 ten times,
+ * more_numbers - Prints the numbers from 0 to 14, ten times,
  *                followed by a new line.
  *
  * Return: void
  */
 void more_numbers(void)
 {
-	int i, num;
+    	int i, num;
 
     	for (i = 0; i < 10; i++)
     	{
         	for (num = 0; num <= 14; num++)
         	{
-            		if (num > 9)
-            		{
-                		char tens = (num / 10) + '0';
-                		write(1, &tens, 1);
-            		}
-            		char ones = (num % 10) + '0';
-            		write(1, &ones, 1);
+            	if (num > 9)
+                	_putchar((num / 10) + '0');
+            	_putchar((num % 10) + '0');
         	}
-        	write(1, "\n", 1);
-    	}
+        	_putchar('\n');
+	}
 }
-
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+#ifndef MAIN_H
+#define MAIN_H
+
 void more_numbers(void);
+int _putchar(char c);
+
+#endif /* MAIN_H */
 void print_most_numbers(void);
 void print_numbers(void);
