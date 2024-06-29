442e4b42b8c3fa7fa1e740711aea84a6f344341f
Abdulquadri31 <abdulquadriakinde@gmai.com>
2024-06-29 03:06:45 +0100

    calender

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int largest_number(int a, int b, int c);
+
+#endif /* MAIN_H */
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
+
+#endif /* MAIN_H */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
