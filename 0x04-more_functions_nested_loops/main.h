a3c75ebf69015e8a542a168c8fd563ff85978ffe
Abdulquadri31 <abdulquadriakinde@gmai.com>
2024-06-29 01:45:08 +0100

    phew

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,3 @@
-int _isdigit(int c);
-#ifndef MAIN_H
-#define MAIN_H
-
-int _isupper(int c);
-
-#endif /* MAIN_H */
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -12,3 +5,28 @@ void print_number(int n);
 int _putchar(char c);
 
 #endif /* MAIN_H */
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void fizz_buzz(void);
+void print_triangle(int size);
+void print_numbers(void);
+void print_most_numbers(void);
+int mul(int a, int b);
+int _isdigit(int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_number(int n);
+int _putchar(char c);
+
+#endif /* MAIN_H */
+
